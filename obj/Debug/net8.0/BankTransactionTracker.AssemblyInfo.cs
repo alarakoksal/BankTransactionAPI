599edfd51779d4d065616a6d19bfeb6f94ec34be
alarakoksal <alarakoksal75@gmail.com>
2026-01-26 23:05:28 +0300

    Ignore local database and build folders

--- obj/Debug/net8.0/BankTransactionTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankTransactionTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankTransactionTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04fd003161aa76daa501cc959280d6aff349fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankTransactionTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankTransactionTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
